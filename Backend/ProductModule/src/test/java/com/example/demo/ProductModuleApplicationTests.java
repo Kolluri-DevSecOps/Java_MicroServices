f82b3ba4a406a9728a33fe3ce387022d03d73551
bharathkumar-rjay <bharath.k@rjaytechnologies.com>
2024-10-28 14:19:37 +0530

    test issue solved

--- Backend/ProductModule/src/test/java/com/example/demo/ProductModuleApplicationTests.java
+++ Backend/ProductModule/src/test/java/com/example/demo/ProductModuleApplicationTests.java
@@ -3,7 +3,10 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.product.ProductModuleApplication;
+
+
+@SpringBootTest(classes = ProductModuleApplication.class)
 class ProductModuleApplicationTests {
 
 	@Test
